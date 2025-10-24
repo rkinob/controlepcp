9a5b188673d4db013b4e5ff8540a7bd02f88a12a
Rodrigo Kinob Torres Campos <rodrigo.campos@gpnet.com.br>
2025-10-24 16:07:47 -0300

    feat: colocar footer com texto da versão e empresa

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotificationComponent } from './components/notification/notification.component';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, NotificationComponent],
+  imports: [RouterOutlet, NavbarComponent, NotificationComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class FooterComponent {
+  version = '1.0.0';
+  currentYear = new Date().getFullYear();
+}
